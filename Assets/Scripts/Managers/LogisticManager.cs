fa49bb097a29fa2a8d4e6e94a4a5e8d6e8e5428a
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-09-18 14:55:23 +0500

    Update LogisticManager.cs

--- Assets/Scripts/Managers/LogisticManager.cs
+++ Assets/Scripts/Managers/LogisticManager.cs
@@ -5,6 +5,9 @@ public class LogisticManager : MonoBehaviour
 {
     public static LogisticManager Instance;
 
+    public List<Logist> AllLogists = new List<Logist>();
+    public Queue<TransportTask> PendingTasks = new Queue<TransportTask>();
+
     private void Awake()
     {
         if (Instance == null)
@@ -14,6 +17,52 @@ public class LogisticManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
+        }
+    }
+
+    public void RegisterLogist(Logist logist)
+    {
+        AllLogists.Add(logist);
+    }
+
+    public void AddTransportTask(Product product, Machine sourceMachine)
+    {
+        TransportTask newTask = new TransportTask
+        {
+            Product = product,
+            SourceMachine = sourceMachine,
+            Status = TaskStatus.Pending
+        };
+        
+        PendingTasks.Enqueue(newTask);
+        AssignTasks();
+    }
+
+    public void UpdateLogistics(float deltaTime)
+    {
+        foreach (Logist logist in AllLogists)
+        {
+            if (logist.IsAvailable)
+            {
+                AssignTasks();
+            }
+            else
+            {
+                logist.UpdateMovement(deltaTime);
+            }
+        }
+    }
+
+    private void AssignTasks()
+    {
+        foreach (Logist logist in AllLogists)
+        {
+            if (logist.IsAvailable && PendingTasks.Count > 0)
+            {
+                TransportTask task = PendingTasks.Dequeue();
+                logist.AssignTask(task);
+                task.Status = TaskStatus.InProgress;
+            }
         }
     }
 }
