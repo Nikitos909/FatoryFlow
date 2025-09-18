4948fe970180847aa83bbfa218e04e50ed44e1f3
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-09-18 14:43:47 +0500

    Update GameManager.cs

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -27,13 +27,47 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }    
+    }
+
+    private void InitializeManagers()
+    {
+        // Загружаем конфиги
+        Config = Resources.Load<GameConfig>("Configs/GameConfig");
+        
+        // Создаем данные игры
+        Data = new GameData();
+        
+        // Инициализируем сервисы
+        DifficultyService = new DifficultyService();
+        
+        // Получаем ссылки на менеджеры
+        ProductionManager = GetComponent<ProductionManager>();
+        LogisticsManager = GetComponent<LogisticsManager>();
+        EconomyManager = GetComponent<EconomyManager>();
+        UIManager = GetComponent<UIManager>();
+        
+        // Запускаем игру
+        StartGame();
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        ProductionManager.UpdateProduction(Time.deltaTime);
+        LogisticsManager.UpdateLogistics(Time.deltaTime);
+    }
+
+    public void SaveGame()
+    {
+        string json = JsonUtility.ToJson(Data);
+        System.IO.File.WriteAllText(Application.persistentDataPath + "/save.json", json);
+    }
+
+    public void LoadGame()
+    {
+        if (System.IO.File.Exists(Application.persistentDataPath + "/save.json"))
         {
-            EconomyManager.AddMoney(10);
+            string json = System.IO.File.ReadAllText(Application.persistentDataPath + "/save.json");
+            Data = JsonUtility.FromJson<GameData>(json);
         }
     }
 }
