bed54f07ed0d2fd8e4338ab995fa9a98a95f2aaf
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-09-22 14:00:11 +0500

    Update MachineData.cs

--- Assets/Scripts/Data/MachineSaveData.cs
+++ Assets/Scripts/Data/MachineSaveData.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// Классы для сохранения состояния объектов на сцене
 [Serializable]
-public class MachineSaveData
+public struct MachineData
 {
-    public string MachineId; // Уникальный ID для связи с префабом
-    public float PositionX;
-    public float PositionY;
-    public int Level; // Уровень апгрейда
-    public bool IsBroken;
-    // ... любые другие данные станка
+    public string machineTypeId;
+    public Vector2 position;
+    public Quaternion rotation;
+    public float age;
+    public int upgradeLevel;
+    public bool isBroken;
+    public float repairProgress;
 }
