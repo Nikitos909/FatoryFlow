6945356fab6d139b1f323cc10097bead44e6f8b8
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-09-22 14:56:45 +0500

    Create ProgressBar.cs

---
+++ Assets/Scripts/Managers/ProgressBar.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public Image fillImage;
+    
+    public void SetProgress(float progress)
+    {
+        fillImage.fillAmount = Mathf.Clamp01(progress);
+    }
+}
