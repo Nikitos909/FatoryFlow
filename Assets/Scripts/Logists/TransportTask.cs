f775d745fc2c6b1f226d15bb7885aaace9874495
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-09-26 13:36:58 +0500

    Update TransportTask.cs

--- Assets/Scripts/Logists/TransportTask.cs
+++ Assets/Scripts/Logists/TransportTask.cs
@@ -1,7 +1,21 @@
+using System;
+using UnityEngine;
 
+[System.Serializable]
 public class TransportTask
 {
-    public Machine fromMachine;
-    public Machine toMachine;
+    public Machine sourceMachine;
+    public Machine destinationMachine;
     public ProductType productType;
+    public int priority; // 1 - высокий, 2 - средний, 3 - низкий
+    public float timestamp; // время создания задачи
+    
+    public TransportTask(Machine source, Machine dest, ProductType type, int prio = 2)
+    {
+        sourceMachine = source;
+        destinationMachine = dest;
+        productType = type;
+        priority = prio;
+        timestamp = Time.time;
+    }
 }
