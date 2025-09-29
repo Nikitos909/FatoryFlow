3e0843a52d4a8ec70c381baa576be837e7460957
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-09-29 14:46:28 +0500

    Update Enums.cs

--- Assets/Scripts/Machines/Enums.cs
+++ Assets/Scripts/Machines/Enums.cs
@@ -1,8 +1,3 @@
 public enum MachineType { Cutter, Bender, Welder, QualityControl }
 public enum ProductType { RawPipe, BentSector, FinalProduct, DefectiveProduct }
-
-public enum WorkerType
-{
-    Logist,     // Логист
-    Controller  // Контролер качества
-}
+public enum WorkerType { Logist, Controller }
