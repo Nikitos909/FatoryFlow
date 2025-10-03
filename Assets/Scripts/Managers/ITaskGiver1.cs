1d89cd39dbf1f8667fe7541281bc9917bf5545bd
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-10-03 13:18:09 +0500

    Update ITaskGiver1.cs

--- Assets/Scripts/Managers/ITaskGiver1.cs
+++ Assets/Scripts/Managers/ITaskGiver1.cs
@@ -1,4 +1,4 @@
-public interface ITaskGiver
+public interface ITaskGiver1
 {
     void OnTaskCompleted(TransportTask1 task); // Вызывается, когда логист выполнил задание
     Vector3 GetPosition(); // Получить позицию для логиста
