be2f79a833092933c2ba0b3d0dd7d2a37a5bff57
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-10-09 20:36:15 +0500

    setting_parameters

--- Assets/Scripts/Managers/ITaskGiver1.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface ITaskGiver1
-{
-    void OnTaskCompleted(TransportTask1 task); // Вызывается, когда логист выполнил задание
-    Vector3 GetPosition(); // Получить позицию для логиста
-}
