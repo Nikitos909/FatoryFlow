b6c80bdf993210798346f6ed2b5115aa773aee8f
Nikita Deryabin <70952862+Nikitos909@users.noreply.github.com>
2025-10-21 16:08:35 +0500

    Update Product.cs

--- Assets/Scripts/Product/Product.cs
+++ Assets/Scripts/Product/Product.cs
@@ -6,12 +6,14 @@ public class Product : MonoBehaviour
     public Machine producedAt;
     public bool isDefective;
     public int baseValue = 50;
+    public bool isLockedForTransport = false; 
 
     public void Initialize(ProductType productType, Machine machine, bool defective = false)
     {
         type = productType;
         producedAt = machine;
         isDefective = defective;
+        isLockedForTransport = false; 
 
         // Устанавливаем стоимость в зависимости от типа
         baseValue = type switch
@@ -22,4 +24,31 @@ public class Product : MonoBehaviour
             _ => 10
         };
     }
+
+    // Метод для блокировки коллайдера продукта во время траспортировки
+    public void LockForTransport(Logist transporter)
+    {
+        isLockedForTransport = true;
+        
+        // Отключаем коллайдер на время транспортировки
+        Collider2D collider = GetComponent<Collider2D>();
+        if (collider != null)
+        {
+            collider.enabled = false;
+        }
+    }
+
+    // Метод для разблокировки коллайдера продукта после траспортировки
+    public void UnlockAfterDelivery()
+    {
+        isLockedForTransport = false;
+        
+        // Включаем коллайдер обратно
+        Collider2D collider = GetComponent<Collider2D>();
+        if (collider != null)
+        {
+            collider.enabled = true;
+        }
+    }
+
 }
